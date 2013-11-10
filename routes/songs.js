3f453db9f5ef69eee0772413a40fe05f93555ce3
Giovanni Cappellotto <potomak84@gmail.com>
2013-11-10 16:53:24 +0100

    Add test song

--- routes/songs.js
+++ routes/songs.js
@@ -3,9 +3,28 @@ var song = new Song('db/test.git');
 
 // GET /songs/123
 exports.show = function(req, res, next) {
-  song.load(req.params.hash, function(err, song) {
-    if (err && err.code === 'ENOENT') return res.json(404, 'not found');
-    if (err) return next(err);
-    res.json(song);
-  });
+  if (req.params.hash == 'test') {
+    res.json({
+      author: {
+        name: 'Giovanni'
+      },
+      parents: ['123'],
+      tracks: [
+        {
+          url: 'https://dl.dropboxusercontent.com/u/3094976/forknroll/synth.mp3',
+          name: 'Synth'
+        },
+        {
+          url: 'https://dl.dropboxusercontent.com/u/3094976/forknroll/drum.mp3',
+          name: 'Drum'
+        }
+      ]
+    });
+  } else {
+    song.load(req.params.hash, function(err, song) {
+      if (err && err.code === 'ENOENT') return res.json(404, 'not found');
+      if (err) return next(err);
+      res.json(song);
+    });
+  }
 };
